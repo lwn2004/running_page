16c7da4110af56f7d3b8bb31eb2a501eaab0ce73
lwn2004 <linwunian@qq.com>
2024-05-06 13:56:03 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,17 +11,17 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: '跑步记录',
-  siteUrl: 'https://linwn.net',
+  siteUrl: 'https://run.linwn.net',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
     {
       name: '主页',
-      url: 'https://linwn.net',
+      url: 'https://run.linwn.net',
     },
     {
       name: '关于',
-      url: 'https://linwn.net/about',
+      url: 'https://run.linwn.net',
     },
   ],
 };
