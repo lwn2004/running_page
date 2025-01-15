e4a3309babc2dc4631fb62f3601e60cbfebfac86
lwn2004 <linwunian@qq.com>
2025-01-15 18:08:14 +0800

    Update index.tsx

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -19,6 +19,12 @@ const Layout = ({ children }: React.PropsWithChildren) => {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <meta property="og:title" content="Running Page" />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="我跑过了一些地方，希望随着时间推移，地图点亮的地方越来越多.
+不要停下来，不要停下奔跑的脚步." />
+        <meta property="og:image" content="http://q7.itc.cn/images01/20240325/72aef71183264797863dcc151d706902.jpeg" />
+        <meta property="og:url" content="https://run.linwn.net" />
       </Helmet>
       <Header />
       <div className="mb-16 p-4 lg:flex lg:p-16">
